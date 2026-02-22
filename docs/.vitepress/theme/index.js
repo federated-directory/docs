45145ade5ad577aefa63940b932597264b6768c9
Wiebe Niehof <wiebe@federated.directory>
2026-02-22 17:12:41 +0100

    Replace Cloudflare analytics with PostHog

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,29 @@
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
-import '@scalar/api-reference/style.css'
+import DefaultTheme from "vitepress/theme";
+import "./custom.css";
+import "@scalar/api-reference/style.css";
 
 export default {
   extends: DefaultTheme,
-  async enhanceApp({ app }) {
+  async enhanceApp({ app, router }) {
     if (!import.meta.env.SSR) {
-      const ScalarWrapper = await import('./ScalarWrapper.vue')
-      app.component('ScalarWrapper', ScalarWrapper.default)
-      const ContactButton = await import('./ContactButton.vue')
-      app.component('ContactButton', ContactButton.default)
-      const ScalarEndpoint = await import('./ScalarEndpoint.vue')
-      app.component('ScalarEndpoint', ScalarEndpoint.default)
+      const { default: posthog } = await import("posthog-js");
+
+      posthog.init("phc_msxVO63thTCGZSj6UUBPcbalPFDVATyKjFGs5fVeJ7w", {
+        api_host: "https://eu.i.posthog.com",
+        person_profiles: "identified_only",
+        capture_pageview: false,
+      });
+
+      router.afterEach(() => {
+        posthog.capture("$pageview", { $current_url: window.location.href });
+      });
+
+      const ScalarWrapper = await import("./ScalarWrapper.vue");
+      app.component("ScalarWrapper", ScalarWrapper.default);
+      const ContactButton = await import("./ContactButton.vue");
+      app.component("ContactButton", ContactButton.default);
+      const ScalarEndpoint = await import("./ScalarEndpoint.vue");
+      app.component("ScalarEndpoint", ScalarEndpoint.default);
     }
-  }
-}
+  },
+};
